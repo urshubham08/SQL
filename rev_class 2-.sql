917c0f6eea1e0f2b275dcdd0e13ac14490f34564
Subham jayasval <259052819+urshubham08@users.noreply.github.com>
2026-05-30 06:28:26 +0530

    Add files via upload

---
+++ rev_class 2-.sql
@@ -0,0 +1,30 @@
+select current_user();
+
+show databases;   -- Show database 
+
+use sakila ;      -- select a database 
+
+show tables;      -- show tables of in current database
+
+select * from customer ;  -- acess data of table 
+
+select customer_id from customer;   -- acces only one 
+
+select customer_id , first_name from customer;    -- acess 2   ,  -- acees many time on colummn also 
+
+select customer_id+10 from customer ; 
+
+-- use database_name
+-- show tables;
+
+-- select * from table_name;
+-- select col, col2, col3 from table;
+
+-- select col1, col2 from table where col=condition
+
+
+
+
+
+ 
+ 
